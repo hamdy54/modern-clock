07d542926c35f1b93d1c4a8971f33a112aa0ac17
hamdy54 <hamdy.ahmed163@yahoo.com>
2023-03-10 03:06:15 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const body = document.querySelector("body"),
+  hourHand = document.querySelector(".hour-hand"),
+  minHand = document.querySelector(".min-hand"),
+  secHand = document.querySelector(".sec-hand"),
+  themeToggleBtn = document.querySelector("#themeToggleBtn");
+
+if (localStorage.getItem("theme") === "light") {
+  body.classList.add("lightIsActive");
+  themeToggleBtn.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+}
+else {
+  body.classList.remove("lightIsActive");
+  themeToggleBtn.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+};
+
+themeToggleBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (localStorage.getItem("theme") === "light") {
+    localStorage.setItem("theme", "dark");
+    themeToggleBtn.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+    body.classList.remove("lightIsActive");
+  } else {
+    localStorage.setItem("theme", "light");
+    themeToggleBtn.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+    body.classList.add("lightIsActive");
+  }
+})
+
+const updateTime = () => {
+  let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360,
+    minToDeg = (date.getMinutes() / 60) * 360,
+    hourToDeg = (date.getHours() / 12) * 360;
+
+  secHand.style.transform = `translateX(-50%) rotate(${secToDeg}deg)`;
+  minHand.style.transform = `translateX(-50%) rotate(${minToDeg}deg)`;
+  hourHand.style.transform = `translateX(-50%) rotate(${hourToDeg}deg)`;
+};
+
+setInterval(() => {
+  updateTime();
+}, 1000);
